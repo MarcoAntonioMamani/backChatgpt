9f04603df257d630ecdf28dad4fa0cd4c183025f
MArco <marcoantoniomamanichura@gmail.com>
2023-11-02 10:27:50 -0400

    cambiando key chat

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mail_jira: 'marcoantoniomamanichura2020@gmail.com',
     toke_jira:'ATATT3xFfGF0fp16f1ogeR_Knlr5v4RtWCDpDiTLtenvN4_FT5HZu1Tlq_vlT3nJ6ALqgFzbdjXtviJL2nzrs8hz3cNtBAFr91oMX2S8mJ8JAL4OPpWhvaMRok4IDVtW_1Zppthkt8fSQobPRPIQDpFigzNeP-OGMLxQgBcW0c0eISSL2o6WrP4=B5B79F3C',
-    key_chatgpt: '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',
+    key_chatgpt: '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',
     model_chatgpt:'gpt-4',
     temperature: 0,
     max_tokens: 1024,
