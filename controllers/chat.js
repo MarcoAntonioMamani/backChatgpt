f11276ab8a8ac52bdf9a0e297eecfe28c3416f42
MArco <marcoantoniomamanichura@gmail.com>
2023-11-03 15:29:55 -0400

    cambios

--- controllers/chat.js
+++ controllers/chat.js
@@ -3,11 +3,74 @@ const config = require('../config')
 let jsonHistoriaUsuario=require('../json/HistoriaUsuario')
 let jsonCasosPruebas=require('../json/CasosPrueba')
 
+
+jsonHistoriaUsuario=`{"historias_usuarios": [
+  {
+    "nombre": "Registro de Usuarios",
+    "descripcion": "Como usuario, quiero poder registrarme en la plataforma para acceder a mis funciones personalizadas.",
+    "criterios_aceptacion": [
+      "Debo poder acceder a la página de registro desde la página de inicio.",
+      "Debo poder ingresar mi nombre, dirección de correo electrónico y contraseña para registrarme.",
+      "Debo recibir un correo electrónico de confirmación después de registrarme.",
+      "Debo poder iniciar sesión con las credenciales que utilicé durante el registro.",
+      "Debo ver un mensaje de bienvenida después de iniciar sesión correctamente."
+    ]
+  },
+  {
+    "nombre": "Gestión de Perfiles de Usuario",
+    "descripcion": "Como administrador, quiero poder gestionar los perfiles de usuario para controlar los accesos y permisos.",
+    "criterios_aceptacion": [
+      "Debo poder agregar nuevos perfiles de usuario.",
+      "Debo poder editar los permisos de los perfiles existentes.",
+      "Debo poder desactivar perfiles de usuario si es necesario.",
+      "Debo ver una lista de todos los perfiles de usuario disponibles."
+    ]
+  }
+]
+}`
+
+jsonCasosPruebas=`{
+  "casos_prueba": [
+    {
+      "titulo": "Acceso a edición de impuestos desde el panel administrativo",
+      "descripcion": "Verificar que el administrador pueda acceder a la sección de edición de impuestos desde el panel administrativo.",
+      "preCondicion": [
+        "El usuario debe estar registrado y autenticado como administrador."
+      ],
+      "pasos": [
+        "Ingresar al sistema con credenciales de administrador.",
+        "Acceder al panel de administración.",
+        "Buscar y seleccionar la opción de edición de impuestos."
+      ],
+      "resultadosEsperados": [
+        "El administrador accede al panel de administración sin problemas.",
+        "La opción de edición de impuestos es visible y accesible."
+      ]
+    },
+    {
+      "titulo": "Modificación del impuesto Percepción AFIP RG 4815",
+      "descripcion": "Verificar que el administrador pueda modificar el porcentaje del impuesto Percepción AFIP RG 4815.",
+      "preCondicion": [
+        "El administrador debe estar en la sección de edición de impuestos."
+      ],
+      "pasos": [
+        "Buscar el impuesto Percepción AFIP RG 4815.",
+        "Cambiar el porcentaje de 35% a 45%.",
+        "Guardar los cambios."
+      ],
+      "resultadosEsperados": [
+        "El sistema permite la modificación del porcentaje sin problemas.",
+        "Los cambios se reflejan correctamente al guardar."
+      ]
+    }
+  ]
+}
+}`
   async function sendMessage(req,res){
 
     console.log(req.body.mensaje)
- jsonHistoriaUsuario=JSON.stringify(jsonHistoriaUsuario)
- jsonCasosPruebas=JSON.stringify(jsonCasosPruebas)
+ //jsonHistoriaUsuario=JSON.stringify(jsonHistoriaUsuario)
+ //jsonCasosPruebas=JSON.stringify(jsonCasosPruebas)
 
 
  
@@ -28,10 +91,12 @@ let jsonCasosPruebas=require('../json/CasosPrueba')
         const response = await axios.request(configChat);
         console.log("-----------------------------------------------------------------------------------------------------------")
         console.log("-----------------------------------------------------------------------------------------------------------")
-       console.log(JSON.parse(response.data.choices[0].message.content))
+        console.log(JSON.parse(response.data.choices[0].message.content))
         return res.status(200).send(JSON.parse(response.data.choices[0].message.content))
 
       } catch (error) {
+        console.log('ERORRR----------------------------------')
+        console.log(response.data.choices[0].message.content)
         console.error(error);
         res.status(500).send({code:1,message: 'Hubo En error al consultar el servicio'})
       }
--- controllers/jira.js
+++ controllers/jira.js
@@ -1,9 +1,17 @@
 const axios = require('axios');
 const config = require('../config');
 
+
+  function isHistoriaUsuario(json) {
+    return 'criterios' in json;
+}
+function isCasosPrueba(json) {
+    return 'pasos' in json && 'resultados' in json;
+}
+
 async function createTestCase(req,res){
     
-   console.log(JSON.stringify(req.body))
+    console.log(JSON.stringify(req.body))
     console.log("------------------------------")
     const url = 'https://ecosistemas22.atlassian.net/rest/api/3/issue';
     const encodedToken = Buffer.from(`${config.mail_jira}:${config.toke_jira}`).toString('base64');
@@ -14,22 +22,41 @@ async function createTestCase(req,res){
             'Content-Type': 'application/json'
         }
     };
-    let historias=req.body
-    for (let historia of historias) {
-        // Combinar descripción y criterios de aceptación
-        let descripcionCompleta = historia.descripcion + "\n\nCriterios de Aceptación:\n";
-        for (let criterio of historia.criterios) {
-            descripcionCompleta += "- " + criterio + "\n";
-        }
-       // console.log(descripcionCompleta)
 
-        // Construir datos para la petición
+  
+
+    let historias = req.body;
+    for (let item of historias) {
+        let descripcionCompleta = "";
+
+        // Formatear la descripción según el formato del JSON
+        if (isHistoriaUsuario(item)) {
+            descripcionCompleta = item.descripcion + "\n\nCriterios de Aceptación:\n";
+            for (let criterio of item.criterios) {
+                descripcionCompleta += "- " + criterio + "\n";
+            }
+        } else if (isCasosPrueba(item)) {
+            descripcionCompleta = item.descripcion + "\n\nPrecondiciones:\n";
+            for (let precondicion of item.precondiciones) {
+                descripcionCompleta += "- " + precondicion + "\n";
+            }
+            descripcionCompleta += "\nPasos:\n";
+            for (let paso of item.pasos) {
+                descripcionCompleta += "- " + paso + "\n";
+            }
+            descripcionCompleta += "\nResultados esperados:\n";
+            for (let resultado of item.resultados) {
+                descripcionCompleta += "- " + resultado + "\n";
+            }
+        }
+
+        // Construcción del objeto para la solicitud
         const datos = {
             fields: {
                 project: {
                     key: 'PER'
                 },
-                summary: historia.nombre,
+                summary: item.nombre,
                 description: {
                     version: 1,
                     type: 'doc',
@@ -51,18 +78,18 @@ async function createTestCase(req,res){
             }
         };
 
-        // Hacer la petición a Jira para crear el testcase
+        // Intentar crear la tarjeta en Jira
         try {
             const response = await axios.post(url, datos, configuracion);
-            //let data =JSON.stringify(response)
-            console.log(`Testcase creado para la historia: ${historia.nombre}  `);
+            console.log(`Testcase creado para: ${item.nombre}`);
         } catch (error) {
-            console.error(`Error creando testcase para la historia: ${historia.nombre}`);
+            console.error(`Error creando testcase para: ${item.nombre}`);
             console.error(error);
-            res.status(500).send({code:1,message: 'Hubo En error al consultar el servicio'})
+            res.status(500).send({code:1,message: 'Hubo un error al consultar el servicio'});
+            return; // Salir del bucle si hay un error
         }
     }
-    res.status(200).send({code:1,message: 'Todos Los item en jira fueron creados correctamente'})
+    res.status(200).send({code:1,message: 'Todos los items en Jira fueron creados correctamente'});
 }
 
 
